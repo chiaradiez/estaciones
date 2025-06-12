0a8127dca23539221983cc899cc5848d293f12b6
chiaradiez <diezchiara@gmail.com>
2025-06-12 10:13:40 -0300

    modifico test

--- obtenerEstacion.test.js
+++ obtenerEstacion.test.js
@@ -2,7 +2,7 @@ const obtenerEstacion = require("./obtenerEstacion");
 
 describe("obtenerEstacion", () => {
   test("Meses de verano", () => {
-    expect(obtenerEstacion("diciembre")).toBe("Verano");
+    expect(obtenerEstacion("diciembre")).toBe("Invierno");
     expect(obtenerEstacion("enero")).toBe("Verano");
     expect(obtenerEstacion("febrero")).toBe("Verano");
   });
